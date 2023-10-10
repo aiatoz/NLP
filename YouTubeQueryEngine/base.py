3e574f7533d7fe8e0107596a198000c407bc5ce2
aiatoz <125128688+aiatoz@users.noreply.github.com>
2023-10-10 12:53:50 +0530

    Create base.py

---
+++ YouTubeQueryEngine/base.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 10 12:40:20 2023
+
+@author: Krishna
+"""
+
+#YOUTUBE QUERYING ENGINE
+#Get the transcript and ground
+#LLAMA2 - Selenium - Extract data and query the text - Instant grounding
